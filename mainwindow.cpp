b780dc73d90e30121e5d098c00784cc07d414bb2
rlee287 <rlee287@yahoo.com>
2018-07-16 23:13:31 -0700

    Continue implementing slider/display logic

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+
 #include "ui_mainwindow.h"
 
 #include <iostream>
@@ -6,8 +7,9 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
 
     // MainWindow takes ownership using QPointer
-    checkPIDs = new QDoubleClipValidator(0, 2, 10, this);
-    checkdt = new QDoubleClipValidator(0.001, 0.1, 10, this);
+    // Keep separate in case this is changed later
+    checkPIDs = new QDoubleValidator(this);
+    checkdt = new QDoubleValidator(this);
 
     ui->setupUi(this);
 
@@ -33,6 +35,10 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     connect(ui->Kd_text, &QLineEdit::textChanged, this, &MainWindow::updateSliders);
 }
 
+static float clamp(float in, float lower, float upper) {
+    return std::max(lower, std::min(in, upper));
+}
+
 void MainWindow::updateLineEdits() {
     if (!changingText) {
         double Kp_val = (double)ui->Kp_slider->value() / (ui->Kp_slider->maximum() + 1);
@@ -43,29 +49,48 @@ void MainWindow::updateLineEdits() {
         Ki_val *= MainWindow::Ki_slider_multiplier;
         Kd_val *= MainWindow::Kd_slider_multiplier;
 
-        ui->Kp_text->setText(QString(std::to_string(Kp_val).c_str()));
-        ui->Ki_text->setText(QString(std::to_string(Ki_val).c_str()));
-        ui->Kd_text->setText(QString(std::to_string(Kd_val).c_str()));
+        ui->Kp_text->setText(QString::number(Kp_val));
+        ui->Ki_text->setText(QString::number(Ki_val));
+        ui->Kd_text->setText(QString::number(Kd_val));
     }
 }
+
 void MainWindow::updateSliders() {
-    double Kp_val = ui->Kp_text->text().toDouble();
-    double Ki_val = ui->Ki_text->text().toDouble();
-    double Kd_val = ui->Kd_text->text().toDouble();
+    // Ignore the boxes with invalid inputs
+    bool Kp_OK, Ki_OK, Kd_OK;
+    double Kp_val = ui->Kp_text->text().toDouble(&Kp_OK);
+    double Ki_val = ui->Ki_text->text().toDouble(&Ki_OK);
+    double Kd_val = ui->Kd_text->text().toDouble(&Kd_OK);
 
-    Kp_val /= MainWindow::Kp_slider_multiplier;
-    Ki_val /= MainWindow::Ki_slider_multiplier;
-    Kd_val /= MainWindow::Kd_slider_multiplier;
+    if (Kp_OK) {
+        Kp_val = clamp(Kp_val, 0, 2);
+        ui->Kp_text->setText(QString::number(Kp_val));
+        Kp_val /= MainWindow::Kp_slider_multiplier;
+        Kp_val *= (ui->Kp_slider->maximum() + 1);
+        changingText = true;
+        ui->Kp_slider->setValue((int)Kp_val);
+        changingText = false;
+    }
+    if (Ki_OK) {
+        Ki_val = clamp(Ki_val, 0, 0.5);
+        ui->Ki_text->setText(QString::number(Ki_val));
+        Ki_val /= MainWindow::Ki_slider_multiplier;
+        Ki_val *= (ui->Ki_slider->maximum() + 1);
 
-    Kp_val *= (ui->Kp_slider->maximum() + 1);
-    Ki_val *= (ui->Ki_slider->maximum() + 1);
-    Kd_val *= (ui->Kd_slider->maximum() + 1);
+        changingText = true;
+        ui->Ki_slider->setValue((int)Ki_val);
+        changingText = false;
+    }
+    if (Kd_OK) {
+        Kd_val = clamp(Kd_val, 0, 1);
+        ui->Kd_text->setText(QString::number(Kd_val));
+        Kd_val /= MainWindow::Kd_slider_multiplier;
+        Kd_val *= (ui->Kd_slider->maximum() + 1);
 
-    changingText = true;
-    ui->Kp_slider->setValue((int)Kp_val);
-    ui->Ki_slider->setValue((int)Ki_val);
-    ui->Kd_slider->setValue((int)Kd_val);
-    changingText = false;
+        changingText = true;
+        ui->Kd_slider->setValue((int)Kd_val);
+        changingText = false;
+    }
 }
 
 MainWindow::~MainWindow() {
